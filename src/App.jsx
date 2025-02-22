d9332660dc45b2caad42f635c646b618879d86ae
rajesh <ajoypal.raj@gmail.com>
2025-02-22 22:32:06 +0600

    Import Error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import TechnicalProficiency from './sections/TechnicalProficiency'
-import AboutMe from './sections/Aboutme'
+
 import MyProjects from './sections/MyProjects'
 import ContactMe from './sections/ContactMe'
 import Footer from './sections/Footer'
+import AboutMe from './sections/AboutMe'
 
 
 const App = () => {
